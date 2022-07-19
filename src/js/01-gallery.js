4d94e9a63d198b2d37022978559576b70971ef51
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-07-20 01:13:51 +0300

    add3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const divContainer = document.querySelector('.gallery');
+// const imgTeg = document.querySelector('.gallery__image');
+const imgCard = getImgCart(galleryItems);
+
+divContainer.insertAdjacentHTML('beforeend', imgCard);
+
+function getImgCart(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </div>
+    `
+}).join("");
+}
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
 
 console.log(galleryItems);
