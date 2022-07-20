8e32dd09c9d427214ca6967f61762f21adc615b1
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-07-20 22:26:50 +0300

    add20.06-4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,39 @@
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 console.log(throttle);
+
+
+    const iframe = document.querySelector('iframe');
+   const player = new Player('iframe', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+
+iframe.addEventListener('timeupdate', function(){
+    const timeControl = parseInt(iframe.currentTime);
+    console.log(timeControl);
+});
+
+
+console.log(localStorage);
+
+// localStorage.setItem('videoplayer-current-time', 'timeControl');
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
