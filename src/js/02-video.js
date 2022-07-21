f13ca355c39772baa1c5c92bdc24f379b776fae0
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-07-21 09:34:21 +0300

    add21.06-1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,36 +4,45 @@ console.log(throttle);
 
 
     const iframe = document.querySelector('iframe');
-   const player = new Player('iframe', {
-    id: 19231868,
-    width: 640
-});
+const player = new Player('iframe');
 
-player.on('play', function() {
+player.on('play', function () {
     console.log('played the video!');
 });
+    // , {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
 
 
-iframe.addEventListener('timeupdate', function(){
-    const timeControl = parseInt(iframe.currentTime);
-    console.log(timeControl);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 console.log(localStorage);
 
-// localStorage.setItem('videoplayer-current-time', 'timeControl');
+player.on('timeupdate', function() {});
 
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
+iframe.addEventListener('timeupdate', function(){
+    const timeControl = parseInt(iframe.target.currentTime);
+    console.log(timeControl);
+});
 
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
+
+
+
