f7b8c77425a018116127ebefaf3ee018252ec0d3
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-07-27 01:14:10 +0300

    add27.06-1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -10,58 +10,13 @@ player.on('play', function() {
     console.log('played the video!');
 });
 
- if(localStorage.currentTime) {
-       player.currentTime = localStorage.currentTime
-    }
 
-    player.addEventListener('timeupdate', () => {localStorage.currentTime = player.currentTime
-});
+player.on('timeupdate', throttle(setTime, 1000));
 
-// player.on('timeupdate', function (time) {
-//     console.log("time", time);
-// });
+function setTime({seconds}) {
+    console.log("time", seconds);
+     localStorage.setItem('videoplayer-current-time', seconds);
+};
 
-// player.on('timeupdate', () => {
-//     localStorage.currentTime = player.currentTime
-// });
-
-// setTime();
-
-// player.on('timeupdate', throttle(setTimeToLocale, 1000));
-
-// function setTimeToLocale(e) {}
-
-// function setTime() {}
-
-
-// player.on('timeupdate', throttle(onPlay, 1000));
-
-// function onPlay({ seconds }) {
-//     localStorage.setItem('videoplayer-current-time', seconds);
-//     console.log(seconds);
-// }
-
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
-
-
-
-
-
-
-
-
-
-// console.log(localStorage);
-
-// player.on('timeupdate', function() {});
-
-// iframe.addEventListener('timeupdate', function(){
-//     const timeControl = parseInt(iframe.target.currentTime);
-//     console.log(timeControl);
-// });
-
-
-
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+console.log(form);
+
+
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+console.log(form.elements.email);
+
+updateForm();
+
+function onInput(e) {
+
+    formData[e.target.name] = e.target.value;
+    const formDataStringify = JSON.stringify(formData);
+    // console.log(formDataStringify);
+    localStorage.setItem(STORAGE_KEY, formDataStringify);
+};
+
+function onSubmit(e) {
+    e.preventDefault();
+    console.log('отправка');
+//    formData[e.target.name] = e.target.value;
+    formData.email = e.target.email.value;
+    formData.message = e.target.message.value;
+    console.log(formData);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function updateForm(e) {
+    const saveForm = localStorage.getItem(STORAGE_KEY);
+    const saveFormPars = JSON.parse(saveForm);
+    if (saveForm) {
+       
+        form.elements.email.value = saveFormPars.email;
+        form.elements.message.value = saveFormPars.message;
+     console.log(saveFormPars);
+    };
+};
