c170585ec22b95da617041552aa011d50fcefee1
Elena-Holz <97180066+Elena-Holz@users.noreply.github.com>
2022-08-02 01:25:48 +0300

    add2.08-1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -24,20 +24,6 @@ function onSubmit(e) {
     localStorage.removeItem(STORAGE_KEY);
 };
 
-// function updateForm(e) {
-//     const saveForm = localStorage.getItem(STORAGE_KEY);
-//     if (saveForm) {
-//         const saveFormPars = JSON.parse(saveForm);
-//         if (saveFormPars.email) {
-//             form.elements.email.value = saveFormPars.email;
-//              console.log(saveFormPars);
-//         };
-//         if (saveFormPars.message) {
-//             form.elements.message.value = saveFormPars.message;
-//              console.log(saveFormPars);
-//         }
-//     }
-// }
 function updateForm(e) {
     const saveForm = localStorage.getItem(STORAGE_KEY);
     const saveFormPars = JSON.parse(saveForm);
